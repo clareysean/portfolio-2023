2d8e23dd4fe51a7a6d694b46a72dbb64db33c4f1
clareysean <seanpclarey@gmail.com>
2023-09-19 13:10:03 -0400

    adjust page behaviour for mobile

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 // Constants
 const burger = document.getElementById('burger');
 const hiddenMenu = document.getElementById('nav-menu-hidden');
+const nav = document.querySelector('.navbar-nav')
 const barLeft = document.querySelector('.bar-left');
 const barRight = document.querySelector('.bar-right');
 const barCenter = document.querySelector('.bar-center');
@@ -18,7 +19,7 @@ function toggleShowMenu(e) {
     } else {
         hiddenMenu.style.right = '-500px';
     }
-
+    nav.classList.toggle('nav-hidden')
     burger.classList.toggle('burger')
     burger.classList.toggle('burger-active')
     // Toggle the 'bar-rotated' class
