fa6aa5004f8d366c54eb068e2b69960c85916d7a
clareysean <seanpclarey@gmail.com>
2023-09-19 12:17:44 -0400

    update styles

---
+++ scripts.js
@@ -0,0 +1,28 @@
+// Constants
+const burger = document.getElementById('burger');
+const hiddenMenu = document.getElementById('nav-menu-hidden');
+const barLeft = document.querySelector('.bar-left');
+const barRight = document.querySelector('.bar-right');
+const barCenter = document.querySelector('.bar-center');
+
+
+let isShowing = false;
+
+// Listeners
+burger.addEventListener('click', toggleShowMenu);
+
+// Functions
+function toggleShowMenu(e) {
+    if (!isShowing) {
+        hiddenMenu.style.right = '0';
+    } else {
+        hiddenMenu.style.right = '-500px';
+    }
+
+    burger.classList.toggle('burger')
+    burger.classList.toggle('burger-active')
+    // Toggle the 'bar-rotated' class
+
+    // Update the isShowing variable
+    isShowing = !isShowing;
+}
