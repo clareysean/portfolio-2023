1a4ece3977bc2af3b0715007af6af0aaf07679c5
clareysean <seanpclarey@gmail.com>
2023-09-20 22:23:25 -0400

    finish prelim mobile styles

--- scripts.js
+++ scripts.js
@@ -5,14 +5,19 @@ const nav = document.querySelector('.navbar-nav')
 const barLeft = document.querySelector('.bar-left');
 const barRight = document.querySelector('.bar-right');
 const barCenter = document.querySelector('.bar-center');
+const navLinksArr = [...document.querySelectorAll('.navlink-h')]
 
 
 let isShowing = false;
 
 // Listeners
+
 burger.addEventListener('click', toggleShowMenu);
 
+navLinksArr.forEach(link=>link.addEventListener('click', toggleShowMenu))
+
 // Functions
+
 function toggleShowMenu(e) {
     if (!isShowing) {
         hiddenMenu.style.right = '0';
@@ -22,8 +27,6 @@ function toggleShowMenu(e) {
     nav.classList.toggle('nav-hidden')
     burger.classList.toggle('burger')
     burger.classList.toggle('burger-active')
-    // Toggle the 'bar-rotated' class
-
-    // Update the isShowing variable
     isShowing = !isShowing;
 }
+
